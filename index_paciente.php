0a45422f0d4886077946af2a4d189c13c2640eed
dolgu <dolgu@Daniel-PC>
2020-03-13 21:52:48 -0600

    Consulta del paciente.

--- index_paciente.php
+++ index_paciente.php
@@ -20,43 +20,44 @@ if(isset($_SESSION['usuario'])){
 
 
         $sql = "SELECT p.pa_n,
-                   u.nombre, 
-                   u.a_paterno, 
-               u.a_materno, 
-               p.enfermedad, 
-                   p.medicamentos, 
-               ts.descripcion, 
-               s.seg_l_desc, 
-               p.peso,
-               p.alergias 
-        from paciente p 
-          inner join usuarios u 
-            on u.user_n = p.user_n_pa
-          inner join cat_tipo_sangre ts
-            on ts.tipo_sangre = p.tipo_sangre
-          inner join cat_seguro s 
-            on s.seg_n = p.seg_n
-        where u.user_n = $user_n";
+                       u.nombre, 
+                       u.a_paterno, 
+                       u.a_materno, 
+                       p.enfermedad, 
+                       p.medicamentos, 
+                       ts.descripcion, 
+                       s.seg_l_desc, 
+                       p.peso,
+                       p.alergias 
+                from paciente p 
+                       inner join usuarios u 
+                       on u.user_n = p.user_n_pa
+                       inner join cat_tipo_sangre ts
+                       on ts.tipo_sangre = p.tipo_sangre
+                       inner join cat_seguro s 
+                       on s.seg_n = p.seg_n
+                where u.user_n = $user_n";
         
-$result = mysqli_query($conexion,$sql);
+$resultado = mysqli_query($conexion,$sql);
 echo "
 	<table border = 1 cellspacing = 1 cellpadding = 1>
 		<tr>
-			<th>ID</th>
+			<th>ID Paciente</th>
 			<th>Nombre</th>
 			<th>Apellidos</th>
 			<th>Edad</th>
 		</tr>";
-while($row = mysqli_fetch_array($result)){
+while($fila = mysqli_fetch_array($resultado)){
 	echo "
 		<tr>
-			<td>".$row[0]."</td>
-			<td>".$row[1]."</td>
-			<td>".$row[2]."</td>
-			<td>".$row[3]."</td>
+			<td>".$fila['pa_n']."</td>
+			<td>".$fila['nombre']."</td>
+			<td>".$fila['a_paterno']."</td>
+			<td>".$fila['a_materno']."</td>
 		</tr>";
 }
 echo "</table>";
+echo "<br/>";
 ?>
 <form action='cerrar_sesion.php'>
     <input type="submit" class="btn btn-danger" name="sesionDestroy" value="Cerrar Sesión"/>
